0254206da5567995d57815fe990b0ecb2e6db0d2
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-28 13:27:36 -0400

    Finished seeds file
    Added seeds file and images to products located under the /public/images folder

--- local-farms/server/config/seeds.js
+++ local-farms/server/config/seeds.js
@@ -1,5 +1,5 @@
 const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const { User, Product, Farm } = require('../models');
 
 db.once('open', async () => {
   await Farm.deleteMany();
@@ -20,8 +20,8 @@ db.once('open', async () => {
     {
       name: 'Apples',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+        'Sweet and juicy apples fresh off our fields.',
+      image: 'apples.jpg',
       farm: farms[0]._id,
       price: 2.99,
       quantity: 500
@@ -29,98 +29,98 @@ db.once('open', async () => {
     {
       name: 'Canned Coffee',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+        'Canned Coffee',
       image: 'canned-coffee.jpg',
-      category: farms[0]._id,
+      farm: farms[0]._id,
       price: 1.99,
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
-      category: farms[1]._id,
+      name: 'Strawberries',
+      farm: farms[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
+        'Strawberries fresh off our fields.',
+      image: 'strawberries.jpg',
       price: 7.99,
       quantity: 20
     },
     {
-      name: 'Handmade Soap',
-      category: farms[1]._id,
+      name: 'Sweet Cherries',
+      farm: farms[1]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
+        'Sweet cherries. Perfect for snacking.',
+      image: 'sweet-cherries.jpeg',
       price: 3.99,
       quantity: 50
     },
     {
-      name: 'Set of Wooden Spoons',
-      category: farms[1]._id,
+      name: 'Tart Cherry Juice',
+      farm: farms[1]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+        'Tart cherry juice is sour to taste and provides a higher amount of anthocyanins compared to black cherry juice, which is sweeter in taste and has less anthocyanins. Anthocyanins promote anti-inflammatory processes in the body. Both are great, nutritious options',
+      image: 'cherry-juice.png',
       price: 14.99,
       quantity: 100
     },
     {
-      name: 'Camera',
-      category: farms[2]._id,
+      name: 'Meyer Lemon Tree',
+      farm: farms[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
+        'Grow your own lemons in your garden!',
+      image: 'lemon-tree.jpg',
+      price: 89.99,
       quantity: 30
     },
     {
-      name: 'Tablet',
-      category: farms[2]._id,
+      name: 'Ornamental Pineapple Plant',
+      farm: farms[2]._id,
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
+        'Aesthetically pleasing pineapple plant for all your home decor needs.',
+      image: 'pineapple-plant.jpg',
+      price: 59.99,
       quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
-      category: farms[3]._id,
+      name: 'Bath Salts with Lavender EO',
+      farm: farms[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
+        'A mix of Himalayan, Dead Sea and Epson salts with pure essential oils of lavender, Bergamot and Ylang Ylang. The lavender essential oil is distilled on our farm. 300 grams.',
+      image: 'bath-salts.jpg',
       price: 9.99,
       quantity: 100
     },
     {
-      name: 'Spinning Top',
-      category: farms[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
+      name: 'Cheddar & Herb Baguette',
+      farm: farms[4]._id,
+      description: 'With a nice crunch to the outside crust to a soft moist inside loaf this cheddar and herb baguette is a perfect combo for a garlic spread loaf. Either fresh or crisped up in the oven this loaf is the perfect addition to your dinner ',
+      image: 'cheddar-baguette.jpg',
+      price: 5.99,
       quantity: 1000
     },
     {
-      name: 'Set of Plastic Horses',
-      category: farms[4]._id,
+      name: 'Cheddar Jalapeno Pretzels',
+      farm: farms[4]._id,
       description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
+        'Soft Old Cheddar and Jalapeño Pretzels. Similar to a pizza bun, only with a pretzel dough. Delicious, very soft and fresh. covered in old melted cheddar and jalapeños.',
+      image: 'cheddar-jalapeno.jpeg',
+      price: 4.99,
       quantity: 1000
     },
     {
-      name: 'Teddy Bear',
-      category: farms[4]._id,
+      name: 'Cheddar Pretzels',
+      farm: farms[4]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
+        'Covered in Melted Old Cheddar. Similar to a Cheddar Pizza bun, Great for school and on the go snack. Fresh and soft!',
+      image: 'cheddar-pretzels.jpg',
+      price: 3.99,
       quantity: 100
     },
     {
-      name: 'Alphabet Blocks',
-      category: farms[4]._id,
+      name: 'Fresh Italian Sausages',
+      farm: farms[4]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
+        'These sausages are excellent on the BBQ or baked in the oven with your favourite potatoes and vegetable. Thick and juicy with so much flavour. They come vacuumed sealed and have never been frozen, fresh ready for the BBQ or ready for freezing.',
+      image: 'italian-sausage.jpg',
       price: 9.99,
       quantity: 600
     }
