686ca34ee6604ca0b3a95ac4eebac7bef7ed5a36
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-28 19:52:08 -0400

    Edited the FarmMenu componenet

--- local-farms/client/src/components/FarmMenu/index.js
+++ local-farms/client/src/components/FarmMenu/index.js
@@ -2,60 +2,60 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from '../../utils/GlobalState';
 import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY,
 } from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+    const [state, dispatch] = useStoreContext();
 
-  const { categories } = state;
+    const { categories } = state;
 
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-  useEffect(() => {
-    if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
-      categoryData.categories.forEach((category) => {
-        idbPromise('categories', 'put', category);
-      });
-    } else if (!loading) {
-      idbPromise('categories', 'get').then((categories) => {
+    useEffect(() => {
+        if (categoryData) {
+            dispatch({
+                type: UPDATE_CATEGORIES,
+                categories: categoryData.categories,
+            });
+            categoryData.categories.forEach((category) => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then((categories) => {
+                dispatch({
+                    type: UPDATE_CATEGORIES,
+                    categories: categories,
+                });
+            });
+        }
+    }, [categoryData, loading, dispatch]);
+
+    const handleClick = (id) => {
         dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id,
         });
-      });
-    }
-  }, [categoryData, loading, dispatch]);
+    };
 
-  const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
-  };
-
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <h2>Choose a Category:</h2>
+            {categories.map((item) => (
+                <button
+                    key={item._id}
+                    onClick={() => {
+                        handleClick(item._id);
+                    }}
+                >
+                    {item.name}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 export default CategoryMenu;
