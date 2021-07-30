408ca538aa27c501f3a5f069345d7764fd4e7fd5
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-30 11:37:54 -0400

    added cart page and component
    the layout for the cart has been added it just needs to be changed so it  can properly update as the user adds items

---
+++ local-farms/client/src/views/examples/CheckoutPage.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import LandingPageHeader from "components/Headers/LandingPageHeader.js";
+import DefaultFooter from "components/Footers/DefaultFooter.js";
+import Cart from "views/index-sections/Cart.js";
+
+function CheckoutPage() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  React.useEffect(() => {
+    document.body.classList.add("product-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("product-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  }, []);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <LandingPageHeader />
+        <div className="wrapper">
+        <Cart />
+        </div>
+        <DefaultFooter />
+      </div>
+    </>
+  );
+}
+
+export default CheckoutPage;
