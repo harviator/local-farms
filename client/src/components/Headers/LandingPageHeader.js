ef2057e0415f01eb898448fe852f1a3793295c9b
Harviator <78506767+harviator@users.noreply.github.com>
2021-08-03 09:02:46 -0400

    Deleted social links on landing page header

--- client/src/components/Headers/LandingPageHeader.js
+++ client/src/components/Headers/LandingPageHeader.js
@@ -35,32 +35,6 @@ function LandingPageHeader() {
         <div className="content-center">
           <Container>
             <h1 className="title">This is our great company.</h1>
-            <div className="text-center">
-              <Button
-                className="btn-icon btn-round"
-                color="info"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <i className="fab fa-facebook-square"></i>
-              </Button>
-              <Button
-                className="btn-icon btn-round"
-                color="info"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <i className="fab fa-twitter"></i>
-              </Button>
-              <Button
-                className="btn-icon btn-round"
-                color="info"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <i className="fab fa-google-plus"></i>
-              </Button>
-            </div>
           </Container>
         </div>
       </div>
